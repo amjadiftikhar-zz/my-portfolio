364c2684c857cba29e381a534d615cd2a811e8cb
amjadiftikhar <amjadiftikhar@github.com>
2020-07-08 14:28:46 +0200

    header changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from "styled-components";
 
 const Style = styled.div`
-    h2 {
+    h1 {
         text-align: center;
         margin-top: 3em;
         color: white;
@@ -14,9 +14,9 @@ const Style = styled.div`
 function Header() {
     return (
         <Style>
-                <h2>
+                <h1>
                 WELCOME TO MY PORTFOLIO
-                </h2>
+                </h1>
         </Style>
     )
 }
